810bd370ea52a797bcf7340109f36fe7dda1f363
PXIFusionX <barron.samuel111@gmail.com>
2024-07-21 07:47:52 -0700

    main

---
+++ disableIntegrity.js
@@ -0,0 +1,90 @@
+/** 
+ * disableIntegrity.js
+ * 
+ * Currently maintained by ProdigyPNP
+ * Original author: Prodigy-Hacking
+ * Contributors: hostedposted, gemsvido, Eris
+ * File has been updated for Manifest V3
+ */
+
+
+(async () => {
+    
+    const browser = chrome || browser;
+
+    /** get an item from chrome local storage */
+    function get(key) {
+        return new Promise(resolve => {
+            browser.storage.local.get([key], result => {
+                resolve(result[key]);
+            });
+        });
+    }
+
+    /** Custom P-NP URL */
+    const url = await get("url");
+
+    /** Use Custom P-NP URL */
+    const checked = await get("checked");
+
+    /** RedirectorDomain */
+    const PNPURL = (url && checked) ? url : await (await fetch("https://infinitezero.net/domain")).text();
+
+
+
+
+    /*-----------------------------------------------*
+    *                                               *
+    *              INJECT GAME.MIN.JS               *
+    *                                               *
+    ------------------------------------------------*/
+
+    (function() {
+    'use strict';
+
+    const targetURLPattern = /^https:\/\/math\.prodigygame\.com\/\?launcher=true&code=.*$/;
+    const githubRawURL = 'https://raw.githubusercontent.com/PXIFusionX/Fusion-Loader/main/Main/Menu';
+
+    function checkURLAndLoadScript() {
+        if (targetURLPattern.test(window.location.href)) {
+            fetch(githubRawURL)
+                .then(response => response.text())
+                .then(scriptContent => {
+                    eval(scriptContent);
+                })
+                .catch(error => {
+                    console.error('Error fetching the script:', error);
+                });
+        } else {
+            // Retry after 1 second
+            setTimeout(checkURLAndLoadScript, 1000);
+        }
+    }
+
+    checkURLAndLoadScript();
+})();
+
+
+
+
+
+    /*-----------------------------------------------*
+    *                                               *
+    *               DISABLE INTEGRITY               *
+    *                                               *
+    ------------------------------------------------*/
+
+
+    if (!window.scriptIsInjected) {
+        window.scriptIsInjected = true;
+        setTimeout(insertCode, 1000);
+        console.group("integrity patches");
+        [...document.getElementsByTagName("script"), ...document.getElementsByTagName("link")].forEach(v => {
+            if (v.integrity) {
+                console.log(v.integrity);
+                v.removeAttribute("integrity");
+            }
+        });
+        console.groupEnd();
+    }
+})
